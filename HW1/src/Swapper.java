80da7d65cd4069cea28bb8e9b9ac6bc104737642
yakovkazinets <yakovkazinets@gmail.com>
2021-02-28 23:59:27 -0500

    HW1

---
+++ HW1/src/Swapper.java
@@ -0,0 +1,18 @@
+public class Swapper implements Runnable {
+    private int offset;
+    private Interval interval;
+    private String content;
+    private char[] buffer;
+
+    public Swapper(Interval interval, String content, char[] buffer, int offset) {
+        this.offset = offset;
+        this.interval = interval;
+        this.content = content;
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run() {
+        // TODO: Implement me!
+    }
+}
---
+++ HW1/src/TextSwap.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class TextSwap {
+
+    private static String readFile(String filename) throws Exception {
+        String line;
+        StringBuilder buffer = new StringBuilder();
+        File file = new File(filename);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        while ((line = br.readLine()) != null) {
+            buffer.append(line);
+        }
+        br.close();
+        return buffer.toString();
+    }
+
+    private static Interval[] getIntervals(int numChunks, int chunkSize) {
+        // TODO: Implement me!
+        return null;
+    }
+
+    private static List<Character> getLabels(int numChunks) {
+        Scanner scanner = new Scanner(System.in);
+        List<Character> labels = new ArrayList<Character>();
+        int endChar = numChunks == 0 ? 'a' : 'a' + numChunks - 1;
+        System.out.printf("Input %d character(s) (\'%c\' - \'%c\') for the pattern.\n", numChunks, 'a', endChar);
+        for (int i = 0; i < numChunks; i++) {
+            labels.add(scanner.next().charAt(0));
+        }
+        scanner.close();
+        // System.out.println(labels);
+        return labels;
+    }
+
+    private static char[] runSwapper(String content, int chunkSize, int numChunks) {
+        List<Character> labels = getLabels(numChunks);
+        Interval[] intervals = getIntervals(numChunks, chunkSize);
+        // TODO: Order the intervals properly, then run the Swapper instances.
+        return null;
+    }
+
+    private static void writeToFile(String contents, int chunkSize, int numChunks) throws Exception {
+        char[] buff = runSwapper(contents, chunkSize, contents.length() / chunkSize);
+        PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+        writer.print(buff);
+        writer.close();
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: java TextSwap <chunk size> <filename>");
+            return;
+        }
+        String contents = "";
+        int chunkSize = Integer.parseInt(args[0]);
+        try {
+            contents = readFile(args[1]);
+            writeToFile(contents, chunkSize, contents.length() / chunkSize);
+        } catch (Exception e) {
+            System.out.println("Error with IO.");
+            return;
+        }
+    }
+}
